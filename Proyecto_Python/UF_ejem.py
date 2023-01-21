de1ffb39090844b143d82014b20f5619afb11883
alexbaezah <alcebaezah@gmail.com>
2023-01-20 21:53:50 -0300

    se modifica uf_ejem.py para obtener el dato de la uf

--- Proyecto_Python/UF_ejem.py
+++ Proyecto_Python/UF_ejem.py
@@ -1,9 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
+#instalar bs4 con pip installa beautifulsoup4
+
 url = 'https://www.bcentral.cl/inicio'                                           #url de bcentral
 html = requests.get(url).content                                                 #Llamar url con método GET
 
 soup = BeautifulSoup(html, 'html.parser')                                        #Extraer datos HTML
-valor_uf = soup.find('div', {'class': 'valor-uf'}).text                          #Traer la class desde código HTML
+
+#acá en la class se entrega la clase que alberga la uf que es:
+# basic-text fs-2 f-opensans-bold text-center c-blue-nb-2
+valor_uf = soup.find('p', {'class': 'basic-text fs-2 f-opensans-bold text-center c-blue-nb-2'}).text                       #Traer la class desde código HTML
 print(valor_uf)
